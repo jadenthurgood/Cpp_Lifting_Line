038b624675395206cf9c797f5486e1b3b3c2da57
Jaden_Thurgood <jadenthurgood7@gmail.com>
2021-07-08 22:09:14 -0400

    updated the main function to read json

--- main.cpp
+++ main.cpp
@@ -1,9 +1,31 @@
+//This code is a c++ translation of my Lifting line code for Potential flow part 1. 
+//I wrote my last semester of grad school. The Python file for this one is called W1
+//The code is based of of Chapter 1 of Warren Phillips Mechanics of Flight book
+
 #include <iostream>
+#include <jsoncpp/json/value.h>
+#include <jsoncpp/json/json.h>
+#include <fstream>
+#include <string>
 
+using namespace std;
+
+//Main function that calls other functions
 int main() {
-    std::cout << "Hello World";
 
-// This is a comment line
+    //Using fstream to get the file pointer in "file"
+    ifstream file("user_input_wing.json");
+    Json::Value actualJson;
+    Json::Reader reader;
+
+    //Using reader we are parsing the Json
+    reader.parse(file, actualJson);
+
+    //Now the actualJson will have the json data
+    cout << "Total json data:\n" << actualJson << endl;
+
+    //Accessing individual parameter
+    cout << "condition" << actualJson["condition"] << endl;
+
     return 0;
 }
-// Here is another line
